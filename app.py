ea197d21d7dcac963230a032e14c9e5ee890f253
WilliamThyer <thyer@uchicago.edu>
2023-02-20 10:26:05 -0600

    alert sound working!

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import PIL.Image as Image, PIL.ImageTk as ImageTk
 import time
 import numpy as np
 from dummy_model import DummyModel
+from pygame import mixer
 
 from fastai.vision.all import *
 from fastai.vision.utils import *
@@ -31,9 +32,12 @@ class App:
         self.button2 = None
         self.pred_probs = []
         self.model = None
+        self.run_preds = True
         self.dummy = dummy
         if dummy:
-            global load_learner 
+            global load_learner
+        mixer.init()
+        self.sound = mixer.Sound("alert.wav")
     
     def load_model(self,model_path = 'edgenext_model.pkl'):
         # load pretrained model
@@ -67,7 +71,7 @@ class App:
         self.create_start_button()
         self.create_stop_button()
         self.add_text()
-        self.create_chart()
+        # self.create_chart()
         self.model = self.load_model()
     
     def add_text(self):
@@ -89,22 +93,37 @@ class App:
         self.video_frame._image_cache = frame # avoid garbage collection
         self.window.after(15, self.show_frame) # 15 ms delay
     
+    def switch_run_preds(self):
+
+        if self.run_preds is False:
+            self.run_preds = True
+        else:
+            self.run_preds = False
+
     def predict(self):
         # predict on webcam feed
-        if self.model is not None:
+        if (self.model) and (self.run_preds):
             pred = self.do_prediction(self.raw_frame)
             pred_prob = float(pred[2][pred[1]])
             self.pred_probs.append(pred_prob)
             pred_str = self.create_output(pred)
+            self.play_sound(pred, pred_prob)
         else:
-            pred_str = "Loading model..."
+            pred_str = "Paused"
         
         self.update_text(pred_str)
+
         self.window.after(500, self.predict)
+    
+    def play_sound(self, pred, pred_prob):
+        if (pred_prob > 0.8) and (pred[0] == 'bad'):
+            print('playing sound')
+            self.sound.play()
 
     def start_webcam(self):
         # start webcam feed
         if self.cap is None:
+            print('Starting webcam...')
             self.cap = cv2.VideoCapture(0) # 0 is the default camera
             self.show_frame()
             self.predict()
@@ -125,14 +144,40 @@ class App:
         self.start_webcam()
         self.window.mainloop()
         
-    def create_chart(self):
-        self.chart = tk.Canvas(self.window, width=200, height=100)
-        self.chart.pack()
-     
-    def update_chart(self):
-        # draw line chart with self.pred_probs
-        self.chart.create_line(0,0,1,1, fill="red", width=2)
-        self.chart.update()
+    # def create_chart(self):
+    #     self.chart = tk.Canvas(self.window, width=200, height=100)
+    #     self.chart.pack()
+    
+    # def create_plot(self):
+    #     # the figure that will contain the plot
+    #     fig = Figure(figsize = (5, 5),
+    #                 dpi = 100)
+    
+    #     # adding the subplot
+    #     plot1 = fig.add_subplot(111)
+
+    # def plot_preds():
+    #     # plotting the graph
+    #     plot1.plot(y)
+    
+    #     # creating the Tkinter canvas
+    #     # containing the Matplotlib figure
+
+    #     canvas = FigureCanvasTkAgg(fig,
+    #                             master = window)  
+    #     canvas.draw()
+    
+    #     # placing the canvas on the Tkinter window
+    #     canvas.get_tk_widget().pack()
+    
+    #     # creating the Matplotlib toolbar
+    #     toolbar = NavigationToolbar2Tk(canvas,
+    #                                 window)
+    #     toolbar.update()
+    
+    #     # placing the toolbar on the Tkinter window
+    #     canvas.get_tk_widget().pack()
+
     
     def create_start_button(self):
         self.button = tk.Button(self.window, text="Start", command=self.start_webcam)
@@ -143,7 +188,7 @@ class App:
         self.button2.pack()
     
     def create_button4(self):
-        self.button4 = tk.Button(self.window, text="Run", command=self.run)
+        self.button4 = tk.Button(self.window, text="Run", command=self.run_preds)
         self.button4.pack()
         
     def __del__(self):
